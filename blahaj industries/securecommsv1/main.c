969a187c5818f5835babe70cab9d8749614c99e7
zuna06 <tanishambala94@gmail.com>
2023-08-18 21:03:11 -0400

    blah

--- blahaj industries/securecommsv1/main.c
+++ blahaj industries/securecommsv1/main.c
@@ -4,7 +4,8 @@
 #include <unistd.h>
 //#include "encryption.c"
 
-int main() {
+
+int main(int argc, char *argv[]) {
 
     printf("-------------------------------------\n");
     printf("GOAT Secure Comms | Blahaj Industries\n");
@@ -14,25 +15,24 @@ int main() {
 
     char key[16];
     char temp[10];
-    char option;
-    char fpath[20];
     char header[4];
     FILE *inFile;
 
+    printf("How To Use:\n");
+    printf("{main} [m]odify/[n]ew infile outfile\n");
+    printf("Example: main m in.txt out.txt\n");
 
     printf("Would you like to modify an existing file or create a new message? [M/n]");
-    fgets(option, 1, stdin);
+    //fgets(option, 1, stdin);
+    printf("%c", *argv[1]);
+
     //scanf("%c", &option);
 
     printf("\n\n");
     
-    if (option == 109 | option == 77) {
+    if (*argv[1] == 109 | *argv[1] == 77) {
 
-        printf("Enter file path: ");
-        scanf("%s", &fpath);
-        
-
-        inFile = fopen("./sample.txt", "r");
+        inFile = fopen(argv[2], "r");
         fread(header, 1, 4, inFile);
 
         printf("%s\n", header);
